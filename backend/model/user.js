d711ba045e3bccbab2806d2c5d336d87044a11cd
amos-kibet <kibetamos97@gmail.com>
2022-12-14 16:46:45 +0300

    chore(enhancement): add additional model fields

--- backend/model/user.js
+++ backend/model/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   name: { type: String, required: false },
-  email: { type: String, required: false },
+  email: { type: String, required: true },
   password: { type: String, required: true },
   courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "course" }],
 });
